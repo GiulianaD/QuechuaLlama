abcf64c693e63c0a29c213366f7577065a8a2e7c
Giuliana Defilippis <giulianadefilippisv1@upb.edu>
2024-10-20 14:38:47 -0400

    DP-001/Normalization Define functions for normalization, including the removal of special characters,extra blank spaces, biblical references and the conversion to lower case.

--- LlamaLLM/dataPreprocessing.py
+++ LlamaLLM/dataPreprocessing.py
@@ -1,6 +1,36 @@
 import pandas as pd
+import re
+import os
+
+# Load the data
 
 splits = {'train': 'data/train-00000-of-00001.parquet', 'validate': 'data/validate-00000-of-00001.parquet', 'test': 'data/test-00000-of-00001.parquet'}
 df = pd.read_parquet("hf://datasets/pollitoconpapass/cuzco-quechua-translation-spanish/" + splits["train"])
 
+#Inspect the data
+
 print(df.head(5))
+print(df.info())
+print(df.shape)
+
+def toLowerCase(input):
+    return input.lower()
+
+def removeSpecialCharacters(input):
+    pattern = r"^\s+|[#\-&$\*\+\[\]\<\>\\=_\*]+"
+    return re.sub(pattern, "", input)
+
+def removeExtraSpaces(input):
+    pattern = r"\s+"
+    return re.sub(pattern, " ", input)
+
+def removeBiblicalReferences(input):
+    pattern = r"\(.+:.*\)"
+    return re.sub(pattern, "", input)
+
+def normalize(input):
+    input = toLowerCase(input)
+    input = removeSpecialCharacters(input)
+    input = removeExtraSpaces(input)
+    input = removeBiblicalReferences(input)
+    return input
