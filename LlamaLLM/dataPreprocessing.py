4b1a033c830d15ce6ff7cd82252411e6d7de8040
Giuliana Defilippis <giulianadefilippisv1@upb.edu>
2024-10-22 16:41:46 -0400

    DP-003/Split the dataset into sentences. Remove the texts whose number of sentences didn't match the number of sentences of their translations. Remove the rows that didn't have the correct language according to their column. Add rows to the dataset

--- LlamaLLM/dataPreprocessing.py
+++ LlamaLLM/dataPreprocessing.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import re
-import os
+import nltk
+from langdetect import detect
+
+nltk.download('punkt')
 
 # Load the data
 
@@ -34,3 +37,46 @@ def normalize(input):
     input = removeExtraSpaces(input)
     input = removeBiblicalReferences(input)
     return input
+
+def split_sentences(text):
+        return nltk.sent_tokenize(text, language='english')
+
+# Split text into sentences and save it as a new dataframe.
+# If the orignal text doesn´t match its translation regarding the number of sentences, then it is discarted.
+
+new_data = []
+
+for i, row in df.iterrows():
+    spanish_sentences = split_sentences(row['spa'])
+    quechua_sentences = split_sentences(row['quz'])
+
+    # Ensure that both sentence lists are of the same length (alignment)
+    if len(spanish_sentences) == len(quechua_sentences):
+        for s_sent, q_sent in zip(spanish_sentences, quechua_sentences):
+            new_data.append({'spanish_sentence': s_sent, 'quechua_sentence': q_sent})
+    else:
+        print(f"Row {i} has misaligned sentences. You may need to handle this case.")
+
+# Create a new DataFrame with the sentence-level data
+# Verify that the spanish column has spanish text and that the 
+# quechua column doesn't have spanish text within it. If so, it is discarted.
+
+df_sentences = pd.DataFrame(new_data)
+indexes2delete = []
+
+for i, row in df_sentences.iterrows():
+    try:
+      lang_sp = detect(row['spanish_sentence'])
+      lang_q = detect(row['quechua_sentence'])
+
+      if lang_sp != 'es' or lang_q == 'es' :
+          indexes2delete.append(i)
+    except:
+      indexes2delete.append(i)
+
+df_sentences.drop(indexes2delete, inplace=True)
+
+# Add new data to the dataset
+df2 = pd.read_csv('./Dataset/EspQuechua.csv')
+df_sentences = pd.concat([df_sentences, df2[['Español', 'Quechua']].rename(columns={'Español': 'spanish_sentence', 'Quechua': 'quechua_sentence'})], ignore_index=True)
+print(df2.shape)
