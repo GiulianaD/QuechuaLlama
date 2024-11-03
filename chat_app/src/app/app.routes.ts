5d0cb7285d1a114aabd33dd0abd94fabd035b530
selwavalencia@gmail.com <selwavalencia@gmail.com>
2024-11-03 12:06:12 -0400

    frontend: Fix app routing.

--- chat_app/src/app/app.routes.ts
+++ chat_app/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent}
--- chat_app/src/app/home/home.component.ts
+++ chat_app/src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatIconModule} from '@angular/material/icon';
-
+import { Message } from '../interfaces/messages';
 
 @Component({
   selector: 'app-home',
@@ -16,4 +16,19 @@ import {MatIconModule} from '@angular/material/icon';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  conversation: Message[] = [];
+  userProfile = 'https://i.pinimg.com/550x/f9/e5/90/f9e590368d01c87a14938ecbb96c97ec.jpg';
+  assistantProfile = 'https://hips.hearstapps.com/hmg-prod/images/shrek-64f9ceef56099.jpg';
+
+  chatInput: string = '';
+
+  sendMessage() {
+    if (this.chatInput.trim()) {
+      this.conversation.push({ role: 'user', content: this.chatInput });
+      const response = `Echo: ${this.chatInput}`;
+      this.conversation.push({ role: 'assistant', content: response });
+      this.chatInput = ''; // Clear input
+    }
+  }
+
 }
